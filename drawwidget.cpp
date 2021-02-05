b3ffd725e6553ce38e8bb3d8f31c195186184491
yhpp <angchen2@outlook.com>
2021-02-05 17:43:12 +0800

    'mouseevent'

--- drawwidget.cpp
+++ drawwidget.cpp
@@ -1,6 +1,49 @@
 #include "drawwidget.h"
 
+#include <QMouseEvent>
+#include <QPainter>
+#include <QPen>
+
 DrawWidget::DrawWidget(QWidget *parent) : QWidget(parent)
 {
+    setAutoFillBackground(true);
+    setPalette(QPalette(Qt::white));
+    pix = new QPixmap(size());
+    pix->fill(Qt::white);
+    setMinimumSize(600,400);
+}
 
+void DrawWidget::setStyle(int s)
+{
+    style = s;
+}
+
+void DrawWidget::setWidth(int w)
+{
+    weight = w;
+}
+
+void DrawWidget::setColor(QColor c)
+{
+    color = c;
+}
+
+void DrawWidget::mousePressEvent(QMouseEvent *e)
+{
+    startPos = e->pos();
+}
+
+void DrawWidget::mouseMoveEvent(QMouseEvent *e)
+{
+    QPainter *painter = new QPainter;
+    QPen pen;
+    pen.setStyle((Qt::PenStyle)style);
+    pen.setWidth(weight);
+    pen.setColor(color);
+    painter->begin(pix);
+    painter->setPen(pen);
+    painter->drawLine(startPos,e->pos());
+    painter->end();
+    startPos = e->pos();
+    update();
 }
--- drawwidget.h
+++ drawwidget.h
@@ -8,10 +8,25 @@ class DrawWidget : public QWidget
     Q_OBJECT
 public:
     explicit DrawWidget(QWidget *parent = nullptr);
+    void mousePressEvent(QMouseEvent *);
+    void mouseMoveEvent(QMouseEvent *);
+    void paintEvent(QPaintEvent *);
+    void resizeEvent(QResizeEvent *);
 
 signals:
 
 public slots:
+    void setStyle(int);
+    void setWidth(int);
+    void setColor(QColor);
+    void clear();
+private:
+    QPixmap *pix;
+    QPoint startPos;
+    QPoint endPos;
+    int style;
+    int weight;
+    QColor color;
 };
 
 #endif // DRAWWIDGET_H
